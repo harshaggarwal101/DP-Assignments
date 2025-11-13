44f3393b34b40ad51de3fd885c7cde653c75e62b
Harsh Aggarwal <harshgmk7968@gmail.com>
2025-11-14 00:35:57 +0530

    Implement perfect sum function in Solution class

--- Assignment-7/GFG-1.cpp
+++ Assignment-7/GFG-1.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    int perfectSum(vector<int>& arr, int target) {
+        int n = arr.size();
+        const int mod = 1e9+7;
+        
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+
+        dp[0][0] = 1;
+        for(int i=1;i<=n;i++) dp[i][0] = 1;
+        for(int j=1;j<=target;j++) dp[0][j] = 0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                int notpick = dp[i-1][j];
+                int pick = 0;
+                if(arr[i-1] <= j){
+                    pick = dp[i-1][j-arr[i-1]];
+                }
+                dp[i][j] = (pick + notpick) % mod;
+            }
+        }
+
+        return dp[n][target];
+    }
+};
 
